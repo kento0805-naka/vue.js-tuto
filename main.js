f44bd087d4536034685f2279e279d636da6e1b0a
kento <kentz.eva.a.r.xxx@gmail.com>
2020-06-13 18:23:44 +0900

    v-bindでstyleの変更

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
-new Vue({
-	el: '#app',
-  data: {
-  	message: 'Hello World!'
-  },
-  methods: {
-  	reverseMessage: function() {
-    	this.message = this.message.split('').reverse().join('')
+var items = [{
+    name: 'pencile',
+    price: 300,
+    quantitiy: 0
+},
+{
+    name: 'note',
+    price: 400,
+    quantitiy: 0
+},
+{
+    name: 'eraser',
+    price: 500,
+    quantitiy: 0
+},
+]
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        items: items
+    },
+    filters: {
+        numberWithDelimiter: function(value) {
+            if (!value) {
+                return 0;
+            }
+            return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+        }
+    },
+    computed: {
+        totalPrice: function() {
+            return this.items.reduce(function(sum, item) {
+                return sum + (item.price * item.quantitiy);
+            }, 0)
+        },
+
+        totalPriceWithTax: function() {
+            return Math.floor(this.totalPrice * 1.10);
+        },
+
+        canBuy: function() {
+            return this.totalPrice >= 1000
+        },
+
+        errorMessageStyle: function() {
+            return {
+                border: this.canBuy ? '' : '1px solid red',
+                color: this.canBuy ? '' : 'red'
+            }
+        }
     }
-  }
 })
+
+
+items[0].quantitiy = 7;
